de1f1d202fbb139fd662cd221f65a3a3f6611053
LAPPLANDQAQ <1504859104@qq.com>
2026-05-28 20:33:36 +0800

    style: rename to runAlgorithmService, use double quotes for consistency

--- frontend/src/api/run.js
+++ frontend/src/api/run.js
@@ -1,6 +1,6 @@
-import http from './http'
+import http from "./http";
 
-export const runAlgorithm = (moduleSlug, payload) => {
-  const slug = String(moduleSlug || '').replaceAll('_', '-')
-  return http.post('/api/algorithms/' + slug + '/run', payload)
-}
+export const runAlgorithmService = (moduleSlug, payload) => {
+  const slug = String(moduleSlug || "").replaceAll("_", "-");
+  return http.post("/api/algorithms/" + slug + "/run", payload);
+};
